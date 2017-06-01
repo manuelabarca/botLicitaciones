15e7b58a7ee6092858faa6822b9758d1d524995c
manuelabarca <labarca.manu@gmail.com>
2017-06-01 16:16:32 +0000

    second commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const APP_TOKEN = 'EAAEV0aMrMXMBAGOqs8qQogap7H1lQFtpE1ztzPVU9qg4i8ZC0WNguJiGtrNUG9iWlT5bzlaSp52iGQH975stZBWj844hh8WevXGmqbmkaUcIKWuNAADpz1zDlS6rQ8AbnknNiHQDBzCmxMqQF1Go8QYT7i85ai3diQ4ZCZBfMwZDZD'
+
+var app = express()
+
+app.use(bodyParser.json())
+
+app.listen(3000, function(){
+	console.log('Server listen localhost:3000')
+})
